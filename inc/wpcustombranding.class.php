4e2db1b5fca45cbf9596eee02bac17ceb776dcbb
Brendan Doyle <brendan@graphedia.ie>
2016-07-12 10:48:31 +0100

    Initial commit

---
+++ inc/wpcustombranding.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @User Brendan
+ * @Package wp-custom-login-branding
+ * @File wpcustombranding.class.php
+ * @Date 12-Jul-16  10:44 AM
+ * @Version 1.0
+ */
+class wpCustomBranding
+{
+
+}
---
+++ wp-custom-login-branding.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: WP Custom Login Branding
+Plugin URI: https://github.com/brendanwex/wp-custom-login-branding
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Brendan Doyle
+Author URI: https://github.com/brendanwex/
+License: A "Slug" license name e.g. GPL2
+*/
+
+
+
